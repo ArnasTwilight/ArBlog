16869feed184e262290fa62d12920bbd9a145615
Arnas <worgenmagic@gmail.com>
2022-06-15 11:36:52 +0300

    add Admin Module

--- config/web.php
+++ config/web.php
@@ -50,6 +50,12 @@ $config = [
             ],
         ],
     ],
+    'modules' => [
+        'admin' => [
+            'class' => 'app\modules\admin\Module',
+        ],
+    ],
+
     'params' => $params,
 ];
 
